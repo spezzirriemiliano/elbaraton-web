441f077b6c58e6b0416e70edb2bd9b33f0322310
Emiliano Spezzirri <spezzirriemiliano@gmail.com>
2018-10-17 20:52:52 -0300

    feat: calculate max value for filters

--- src/app/components/ba-category/ba-filters/ba-filters.component.ts
+++ src/app/components/ba-category/ba-filters/ba-filters.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input} from '@angular/core';
-import { FilterProduct } from '../../../interfaces';
+import { Component, Input, OnChanges} from '@angular/core';
+import { FilterProduct, Product } from '../../../interfaces';
 
 
 @Component({
@@ -7,8 +7,28 @@ import { FilterProduct } from '../../../interfaces';
   templateUrl: './ba-filters.component.pug',
   styleUrls: ['./ba-filters.component.scss']
 })
-export class BaFiltersComponent {
+export class BaFiltersComponent implements OnChanges {
 
     @Input() filters: FilterProduct;
+    @Input() products: Array<Product>;
+
+    minPrice: number = 0;
+    maxPrice: number = 0;
+    maxStock: number = 0;
+
+    ngOnChanges(changes) {
+      if (changes.products && this.products) {
+        this.calculateLimitValues(this.products);
+      }
+    }
+
+    calculateLimitValues(products: Array<Product>) {
+      const prices = products.map(p => p.price);
+      this.maxPrice = Math.max(...prices);
+      this.minPrice = Math.min(...prices);
+      this.maxStock = Math.max(...products.map(p => p.price));
+      console.log('maxPrice: ' + this.maxPrice + ', minPrice: ' + this.minPrice + ', maxStock: ' + this.maxStock);
+    }
+
 
 }
